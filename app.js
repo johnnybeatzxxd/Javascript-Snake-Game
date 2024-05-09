a00d790b8438344b08438b4059c4ee307a1572a6
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-05-09 22:27:49 +0300

    drew the game border

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-var canvas = document.getElementById('gameCanvas');
-var context = canvas.getContext('2d');
+const gameBorder = document.getElementById("gameBorder");
+
+const dot = document.createElement("div");
+dot.style.width = "20px";
+dot.style.height = "20px";
+dot.style.backgroundColor = "red";
+dot.style.position = "absolute";
+gameBorder.appendChild(dot);
+
+let x = 10;
+let y = 10;
+
+function updateDotPosition() {
+    dot.style.left = x + "px";
+    dot.style.top = y + "px";
+}
+
+
+updateDotPosition();
+setInterval(() => {
+    console.log(x, y)
+    x = x + 1;
+
+    updateDotPosition()
+}, 10000)
