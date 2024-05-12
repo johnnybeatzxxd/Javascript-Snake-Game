ffb438487b97cbcfb05883ea29f3f4e422a3f507
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-05-12 19:05:39 +0300

    made the snake movable

--- app.js
+++ app.js
@@ -1,18 +1,85 @@
-const gameBoard = document.getElementById("game-board");
+const gameBoard = document.getElementById("game-board"); // board
+let snakeBody = [{ x: 2, y: 4 }];
+let direction = null;
+let foodPosition = null;
 
-let snakeBody = [{ x: 1, y: 5 }];
+
+
+
+
+
+function drawBoard() {
+    gameBoard.innerHTML = ''; // refresh the board
+    let snake = document.createElement("div");
+
+    // create the snake
+    snakeBody.forEach(body => {
+        snake.style.backgroundColor = "red";
+        // set snake position
+        snake.style.gridColumnStart = body.x;
+        snake.style.gridRowStart = body.y;
+        // add the snake to the board
+        gameBoard.appendChild(snake);
+    });
+
+}
+
+function createFood() {}
 
 function update() {
+    if (direction === "up") {
+        snakeBody.forEach(body => {
+            body.y--;
+        });
+    } else if (direction === "down") {
+        snakeBody.forEach(body => {
+            body.y++;
+        });
+    } else if (direction === "right") {
+        snakeBody.forEach(body => {
+            body.x++;
+        });
+    } else if (direction === "left") {
+        snakeBody.forEach(body => {
+            body.x--;
+        });
+    }
 
-    snakeBody = snakeBody.map(segment => ({ x: segment.x + 1, y: segment.y }));
-    gameBoard.innerHTML = '';
-    snakeBody.forEach(segment => {
-        const snakeElement = document.createElement("div");
-        snakeElement.style.gridRowStart = segment.x;
-        snakeElement.style.gridColumnStart = segment.y;
-        snakeElement.classList.add("snake");
-        gameBoard.appendChild(snakeElement);
+}
+
+function setDirection() {
+    document.addEventListener('keydown', function(event) {
+        // Up arrow
+        if (event.key === 'ArrowUp' && direction !== 'down') {
+            direction = 'up';
+            console.log(direction);
+        }
+        // Down arrow
+        else if (event.key === 'ArrowDown' && direction !== 'up') {
+            direction = 'down';
+            console.log(direction);
+        }
+        // Left arrow
+        else if (event.key === 'ArrowLeft' && direction !== 'right') {
+            direction = 'left';
+            console.log(direction);
+        }
+        // Right arrow
+        else if (event.key === 'ArrowRight' && direction !== 'left') {
+            direction = 'right';
+            console.log(direction);
+        }
     });
 }
 
-setInterval(update, 500); // Corrected to pass the function reference
+function gameOver() {
+    direction = null;
+
+}
+setInterval(() => {
+    drawBoard()
+    update()
+
+}, 200);
+
+setDirection()
